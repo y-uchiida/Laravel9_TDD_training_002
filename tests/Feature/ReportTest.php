30a935bd33a4f422a65a5403fc4b991419c4cd6d
yugo uchiida <y.uchiida@nirneege.com>
2023-01-19 02:26:29 +0900

    そのほかのAPIエンドポイントに対するアクセステストを追加

--- tests/Feature/ReportTest.php
+++ tests/Feature/ReportTest.php
@@ -17,4 +17,67 @@ class ReportTest extends TestCase
         // 検証
         $response->assertOk();
     }
+
+    /** @test */
+    public function api_customersにPOSTメソッドでアクセスできる()
+    {
+        $response = $this->post('api/customers');
+        $response->assertOk();
+    }
+
+    /** @test */
+    public function api_customers_customer_idにGETメソッドでアクセスできる()
+    {
+        $response = $this->get('api/customers/1');
+        $response->assertOk();
+    }
+
+    /** @test */
+    public function api_customers_customer_idにPUTメソッドでアクセスできる()
+    {
+        $response = $this->put('api/customers/1');
+        $response->assertOk();
+    }
+
+    /** @test */
+    public function api_customers_customer_idにDELETEメソッドでアクセスできる()
+    {
+        $response = $this->delete('api/customers/1');
+        $response->assertOk();
+    }
+
+    /** @test */
+    public function api_reportsにGETメソッドでアクセスできる()
+    {
+        $response = $this->get('api/reports');
+        $response->assertOk();
+    }
+
+    /** @test */
+    public function api_reportsにPOSTメソッドでアクセスできる()
+    {
+        $response = $this->post('api/reports');
+        $response->assertOk();
+    }
+
+    /** @test */
+    public function api_reports_report_idにGETメソッドでアクセスできる()
+    {
+        $response = $this->get('api/reports/1');
+        $response->assertOk();
+    }
+
+    /** @test */
+    public function api_reports_report_idにPUTメソッドでアクセスできる()
+    {
+        $response = $this->put('api/reports/1');
+        $response->assertOk();
+    }
+
+    /** @test */
+    public function api_reports_report_idにDELETEメソッドでアクセスできる()
+    {
+        $response = $this->delete('api/reports/1');
+        $response->assertOk();
+    }
 }
