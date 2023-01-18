c9e7c610427039abd44290003bfac1ac1fee9b45
yugo uchiida <y.uchiida@nirneege.com>
2023-01-19 02:59:23 +0900

    factory クラスの生成内容を設定

--- database/factories/ReportFactory.php
+++ database/factories/ReportFactory.php
@@ -17,7 +17,8 @@ class ReportFactory extends Factory
     public function definition()
     {
         return [
-            //
+            'visit_date' => $this->faker->date(),
+            'detail' => $this->faker->realText()
         ];
     }
 }
