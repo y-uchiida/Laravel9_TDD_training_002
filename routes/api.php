c2438a037f941377d4628cbcd11f8ce7436c5dea
yugo uchiida <y.uchiida@nirneege.com>
2023-01-19 06:04:18 +0900

    Get api/customers のJsonレスポンスがテストを通るように最小実装

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Customer;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -18,8 +19,13 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+/**
+ * (仮) Customer の一覧を返す
+ * ある程度挙動が完成したタイミングでルーティングから処理を切り出す
+ */
 Route::get('customers', function () {
-    return response()->json();
+    $customers = Customer::query()->select(['id', 'name'])->get();
+    return response()->json($customers);
 });
 Route::post('customers', function () {
     return;
