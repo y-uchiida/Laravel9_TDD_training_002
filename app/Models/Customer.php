c604da5ad1c0a09b42aeb58cb881b226d21ed8c1
yugo uchiida <y.uchiida@nirneege.com>
2023-01-19 02:46:59 +0900

    モデルにリレーションメソッドを設定

--- app/Models/Customer.php
+++ app/Models/Customer.php
@@ -1,11 +1,19 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Customer extends Model
 {
     use HasFactory;
+
+    public function reports(): HasMany
+    {
+        return $this->hasMany(Report::class);
+    }
 }
